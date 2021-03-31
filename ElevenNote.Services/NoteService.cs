75708df69d31c625b9ed78f9c412cb1ecee1467b
Daniele Thomas <danieleSThomas6@gmail.com>
2021-03-31 11:47:27 -0400

    Adding Note Delete

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -74,8 +74,8 @@ namespace ElevenNote.Services
                         ModifiedUtc = entity.ModifiedUtc
                     };
             }
-        
 
+        }
         public bool UpdateNote(NoteEdit model)
         {
             using (var ctx = new ApplicationDbContext())
@@ -88,6 +88,20 @@ namespace ElevenNote.Services
 
                     return ctx.SaveChanges() == 1;
                         
+            }
+        }
+
+      
+
+        public bool DeleteNote(int noteId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Notes.Single(e => e.NoteId == noteId && e.OwnerId == _userId);
+
+                ctx.Notes.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
             }
         }
 
